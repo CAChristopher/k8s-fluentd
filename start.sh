9f64021096d8e3bd19d270612237d97a5bc50476
Cory Christopher <cory.christopher@i30875-is4.octanner.com>
2016-04-14 15:22:05 -0600

    fixed logging to stdout

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ sed -ie "s,ES_ENDPOINT,${ES_ENDPOINT},g" /etc/td-agent/td-agent.conf
 sed -ie "s/ES_REGION/${ES_REGION}/g" /etc/td-agent/td-agent.conf
 
 
-td-agent $FLUENTD_ARGS -o /var/log/td-agent/td-agent.log 
+td-agent $FLUENTD_ARGS
