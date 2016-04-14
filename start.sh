b51931afafcaaa7c90c9d401ac456b86996533fc
Cory Christopher <cory.christopher@i30875-is4.octanner.com>
2016-04-14 11:55:03 -0600

    remove uneeded vars

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 
 
-#!/usr/bin/env bash
+#!/bin/bash
 
 # SIGTERM-handler
 term_handler() {
@@ -8,11 +8,6 @@ term_handler() {
 }
 
 trap 'kill ${!}; term_handler' SIGTERM
-
-sed -ie "s/ES_AWS_ACCESS_KEY_ID/${ES_AWS_ACCESS_KEY_ID}/g" /etc/td-agent/td-agent.conf
-sed -ie "s/ES_AWS_SECRET_ACCESS_KEY/${ES_AWS_SECRET_ACCESS_KEY}/g" /etc/td-agent/td-agent.conf
-
-set -x
 
 sed -ie "s,ES_ENDPOINT,${ES_ENDPOINT},g" /etc/td-agent/td-agent.conf
 sed -ie "s/ES_REGION/${ES_REGION}/g" /etc/td-agent/td-agent.conf
