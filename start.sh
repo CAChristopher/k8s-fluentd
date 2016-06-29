81062d20a0d58643103d0d8d9faf54c48dd4a559
Cory Christopher <cory.christopher@i30875-is4.octanner.com>
2016-06-29 12:11:25 -0600

    revert

--- start.sh
+++ start.sh
@@ -14,6 +14,5 @@ set -x
 
 sed -ie "s,ES_ENDPOINT,${ES_ENDPOINT},g" /etc/td-agent/td-agent.conf
 sed -ie "s/ES_REGION/${ES_REGION}/g" /etc/td-agent/td-agent.conf
-sed -ie "s/API_URL/${API_URL}/g" /etc/td-agent/td-agent.conf
 
 td-agent $FLUENTD_ARGS
